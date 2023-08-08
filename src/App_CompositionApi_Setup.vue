845b65d888ccbfc0d902c2c4f55f6494df6ced49
yeonhwa <yeonhwa6717@gmail.com>
2023-08-08 20:45:37 +0900

    Composition Api - setup

---
+++ src/App_CompositionApi_Setup.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<!--템플릿 문자열로 보여주기-->
+		<p>{{ message }}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	setup() {
+		// create 되기전 실행
+		const message = 'Hello VUE3' // 선언하고
+
+		return {
+			message, // 리턴해서
+		}
+	},
+}
+</script>
+
+<style lang="scss" scoped></style>
