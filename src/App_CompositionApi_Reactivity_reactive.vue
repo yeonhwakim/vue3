30db05a16e38785eda0c31c2fee08b8f887c83dd
yeonhwa <yeonhwa6717@gmail.com>
2023-08-08 21:29:44 +0900

    Composition api - reactivity - reactive

---
+++ src/App_CompositionApi_Reactivity_reactive.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<button @click="() => reactiveRef.count++">
+			{{ `${reactiveRef.count}` }}
+		</button>
+	</div>
+</template>
+
+<script>
+import { reactive } from 'vue'
+
+export default {
+	setup() {
+		const reactiveRef = reactive({ count: 0 })
+		console.log(reactiveRef.count) // 0
+		reactiveRef.count++
+		console.log(reactiveRef.count) // 1
+
+		return {
+			reactiveRef,
+		}
+	},
+}
+</script>
+
+<style lang="scss" scoped></style>
