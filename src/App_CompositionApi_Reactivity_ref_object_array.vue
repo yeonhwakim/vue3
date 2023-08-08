22a82cf93ed392cbf3364ee9745b6c9283bd954e
yeonhwa <yeonhwa6717@gmail.com>
2023-08-08 21:30:40 +0900

    composition api - reactivity - ref - object / array

---
+++ src/App_CompositionApi_Reactivity_ref_object_array.vue
@@ -0,0 +1,28 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+import { reactive, ref } from 'vue'
+
+export default {
+	setup() {
+		const count = ref(0)
+		count.value++
+
+		// ref -> object
+		const object = reactive({
+			count,
+		})
+		console.log('reactive(object)', object.count) //unwarpping
+
+		// ref -> array
+		const array = reactive([count])
+		console.log('reactive(array)', array[0].value)
+
+		return {}
+	},
+}
+</script>
+
+<style lang="scss" scoped></style>
