1dd969ed04da94aa53840814682a8bd242704ad5
yeonhwa <yeonhwa6717@gmail.com>
2023-08-08 21:28:40 +0900

    Composition api - reactivity - ref

---
+++ src/App_CompositionApi_Reactivity_ref.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<button @click="() => primitiveRef++">
+			{{ `원시값 ${primitiveRef}` }}
+		</button>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+	setup() {
+		const primitiveRef = ref(0)
+		console.log(primitiveRef.value) // 0
+		primitiveRef.value++
+		console.log(primitiveRef.value) // 1
+
+		return {
+			primitiveRef,
+		}
+	},
+}
+</script>
+
+<style lang="scss" scoped></style>
