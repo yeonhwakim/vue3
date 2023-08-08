f6a7e0a93172f184253f3e7392d32bb91fa041b3
yeonhwa <yeonhwa6717@gmail.com>
2023-08-08 20:54:27 +0900

    Composiotion Api - lifecycle hooks

---
+++ src/App_CompositionApi_LifeCycle.vue
@@ -0,0 +1,23 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+import { onBeforeMount, onMounted } from 'vue'
+
+export default {
+	setup() {
+		console.log('setup')
+
+		onMounted(() => {
+			console.log('onMounted')
+		})
+
+		onBeforeMount(() => {
+			console.log('onBeforeMounted')
+		})
+	},
+}
+</script>
+
+<style lang="scss" scoped></style>
